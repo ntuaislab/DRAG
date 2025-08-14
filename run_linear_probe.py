[BugFix] Specify average='micro' in MulticlassAccuracy

run_linear_probe.py
@@ -31,7 +31,9 @@ class LitClassificationHead(LitSplitClassifierBase):
         super().__init__()
 
         self.fc = fc
-        self._metric = MulticlassAccuracy(num_classes=fc.out_features) # type: ignore
+        self._metric = MulticlassAccuracy(
+            num_classes=fc.out_features, average="micro",
+        ) # type: ignore
 
     def unwrap(self) -> nn.Linear:
         return self.fc

runner/detokenization.py
@@ -176,7 +176,7 @@ class DetokenizationRunner(LearningBasedDRA):
         x, y = torch.meshgrid(x, y, indexing='ij')
         coordinates = torch.stack((x, y), dim=-1).unsqueeze(0).to(self.device)
 
-        accuracy = MulticlassAccuracy(1 + P ** 2).to(self.device)
+        accuracy = MulticlassAccuracy(1 + P ** 2, average="micro").to(self.device)
 
         position_err = 0
         dataloader = self.dataloader[VAL]